b2817b21825ea9802030a6eceb21dfc293b7e68c
eduffis <erickduffis@gmail.com>
2017-09-15 11:39:36 -0500

    Update mysqlCop.py

--- mysqlCop.py
+++ mysqlCop.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
+#Autor: Erick Duffis
+#Email:erickduffis@gmail.com
 #This is a machine learning code that search for queries that delay the mysql process and kill them
 #good for system administrators
+#this example only shows a delay method but you can add any amount and train the machine to detect them, 
+#such as detecting attacks by injection
 import time,os
 import MySQLdb
 from sklearn import tree
